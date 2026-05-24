cdcec07cd12d82ad5cb087543ab28678fe199f3e
Pranita1108 <dpranita1108@gmail.com>
2026-05-24 17:45:12 +0530

    Docker.sh

--- docker.sh
+++ docker.sh
@@ -1,23 +1,15 @@
-# update the package list
- sudo apt upgrade -y
+# Update system packages
+sudo dnf update -y
 
-# install docker 
-sudo amazon-linux-extras enable docker
-sudo yum install -y docker
+# Install Docker
+sudo dnf install -y docker
 
-# Add both users to the sudo group (so they can run admin commands)
-sudo usermod -aG sudo ubuntu
-sudo usermod -aG sudo jenkin
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
 
-# apply new group setting immediately
-newgrp docker
+# Add ec2-user to docker group
+sudo usermod -aG docker ec2-user
 
-# set correct permission for the docker socket to allow docker group member to access it
-sudo chmod 660 /var/run/docker.sock
-sudo chown root:docker /var/run/docker.sock
-
-# restart docker service to apply changes
-sudo systemct1 restart docker 
-
-#verify installation
-docker -version
+# Verify installation
+docker --version
