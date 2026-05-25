f00ed663c3e3670f6edb4e3c4bac1f433a114cce
Pranita1108 <dpranita1108@gmail.com>
2026-05-25 15:18:17 +0530

    Deply.sh

---
+++ deply.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Deploying myapp on RHEL..."
+
+# Stop Tomcat (RHEL service-based)
+echo "Stopping Tomcat..."
+sudo systemctl stop tomcat
+
+# Clean old app
+#echo "Cleaning old deployment..."
+#sudo rm -rf /var/lib/tomcat/webapps/ROOT*
+#sudo rm -rf /var/lib/tomcat/webapps/vivekapp*
+
+# Copy new WAR
+#echo "Copying new WAR..."
+sudo cp /tmp/target/vivekapp.war /var/lib/tomcat/webapps/ROOT.war
+
+# Set ownership (important for RHEL)
+sudo chown tomcat:tomcat /var/lib/tomcat/webapps/*
+
+# Start Tomcat
+echo "Starting Tomcat..."
+sudo systemctl start tomcat
+
+echo "Deployment complete!"
